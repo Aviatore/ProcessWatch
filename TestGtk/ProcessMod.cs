3610c104a063bd1f18ca7d65a978b57ee3c2e154
Wojciech Wesołowski <wojtek.wes@gmail.com>
2020-12-26 13:27:29 +0100

    Initial commit
    Working window that displays a list of processes.

---
+++ TestGtk/ProcessMod.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TestGtk
+{
+    public class ProcessMod
+    {
+        public string ProcessName { get; private set; }
+        public double CpuUsage { get; private set; }
+        public double Id { get; private set; }
+        public long WorkingSet64 { get; private set; }
+        public TimeSpan TotalProcessorTime { get; private set; }
+        
+        public ProcessMod()
+        {
+            
+        }
+
+        public static ProcessMod[] GetProcesses()
+        {
+            List<ProcessMod> proc = new List<ProcessMod>();
+            
+            Process[] processes = Process.GetProcesses();
+            
+            List<Task<ProcessMod>> tasks = new List<Task<ProcessMod>>();
+            
+            foreach (var process in processes)
+            {
+                tasks.Add(GetCpuUsageAsync(process));
+            }
+
+            Task task = Task.WhenAll(tasks);
+            task.Wait();
+
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                proc.Add(tasks[i].Result);
+            }
+
+            return proc.ToArray();
+        }
+
+        private static async Task<ProcessMod> GetCpuUsageAsync(Process proc)
+        {
+            DateTime startTime = DateTime.UtcNow;
+            var startCpuUsage = proc.TotalProcessorTime;
+        
+            await Task.Delay(500);
+        
+            DateTime endTime = DateTime.UtcNow;
+            
+            var endCpuUsage = proc.TotalProcessorTime;
+
+            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+
+            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+
+            ProcessMod tmp = new ProcessMod();
+            tmp.ProcessName = proc.ProcessName;
+            tmp.Id = proc.Id;
+            tmp.WorkingSet64 = proc.WorkingSet64;
+            tmp.CpuUsage = cpuUsageTotal * 100 * 4;
+            //Console.WriteLine(tmp.CpuUsage);
+            
+            return tmp;
+        }
+        
+        public static string FormatMemSize(double size)
+        {
+            double d = (double)size;
+            int i = 0;
+            while ((d > 1024) && (i < 5))
+            {
+                d /= 1024;
+                i++;
+            }
+            string[] unit = { "B", "KB", "MB", "GB", "TB" };
+            return $"{Math.Round(d, 2)} {unit[i]}";
+            //return (string.Format("{0} {1}", Math.Round(d, 2), unit[i]));
+        }
+    }
+}
---
+++ TestGtk/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Timers;
+using GLib;
+using Gtk;
+using Application = Gtk.Application;
+using Timer = System.Timers.Timer;
+
+namespace TestGtk
+{
+    class Program
+    {
+        private static Timer _timer = new Timer();
+        static ThreadNotify notify;
+        private static Updater _updater;
+        
+        static void Main(string[] args)
+        {
+            Widget widget;
+            Window window;
+            HBox hbox;
+            VBox vbox;
+            Frame frame;
+            Label label;
+
+            Application.Init ();
+
+
+            window = new Window ("Label sample");
+
+            window.DeleteEvent += delete_event;
+
+            window.Title = "Label";
+            window.BorderWidth = 5;
+            
+            hbox = new HBox (false, 5);
+            vbox = new VBox (false, 5);
+
+            Button button = new Button("Start");
+            button.Clicked += Start;
+            
+            Button button2 = new Button("Stop");
+            button2.Clicked += Stop;
+            
+            hbox.PackStart(button, false, false, 0);
+            hbox.PackStart(button2, false, false, 0);
+            
+            window.Add (vbox);
+            vbox.PackStart (hbox, false, false, 0);
+
+            frame = new Frame ("Processes");
+            VBox processesVBox = new VBox(false, 5);
+            frame.Add(processesVBox);
+
+            vbox.PackStart(frame, false, false, 0);
+            
+            //frame.Add (label);
+
+            //notify = new ThreadNotify(new ReadyEvent(() => { Updater(vbox);}));
+            //Thread thr = new Thread(new ThreadStart(() => { Updater(vbox);}));
+
+            _updater = new Updater();
+            _updater.OnResult += (sender, list) =>
+            {
+                Application.Invoke(delegate
+                {
+                    foreach (var element in processesVBox.Children)
+                    {
+                        processesVBox.Remove(element);
+                    }
+
+                    foreach (var element in list)
+                    {
+                        label = new Label(element);
+                        processesVBox.PackStart(label, false, false, 0);
+                    }
+                    
+                    window.ShowAll();
+                    
+                });
+            };
+
+/*
+            _timer.Interval = 1000;
+            //_timer.AutoReset = false;
+            _timer.Elapsed += (sender, eventArgs) =>
+            {
+                Updater(vbox);
+            };
+            _timer.Start();
+*/
+            window.ShowAll();
+
+            //thr.Start();
+            Application.Run();
+        }
+
+        static void Start(object sender, EventArgs args)
+        {
+            _updater.Run();
+        }
+        
+        static void Stop(object sender, EventArgs args)
+        {
+            _updater.Stop();
+        }
+
+        static void delete_event (object obj, DeleteEventArgs args)
+        {
+            _updater.Stop();
+            Application.Quit();
+        }
+ 
+        static void exitbutton_event (object obj, EventArgs args)
+        {
+            Application.Quit();
+        }
+    }
+
+    public class Updater
+    {
+        public event EventHandler<List<string>> OnResult;
+        private Timer aTimer;
+        
+        public Updater()
+        {
+            
+        }
+
+        public void Run()
+        {
+            SetTimer();
+        }
+
+        public void Stop()
+        {
+            aTimer.Stop();
+            aTimer.Dispose();
+        }
+
+        private void SetTimer()
+        {
+            aTimer = new Timer(1000);
+            aTimer.Elapsed += GetData;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        
+        private void GetData(object source, ElapsedEventArgs args)
+        {
+            Console.WriteLine("ok");
+            List<string> output = new List<string>();
+            ProcessMod[] processes = ProcessMod.GetProcesses();
+            IEnumerable<ProcessMod> processesSorted = processes.OrderByDescending(process => process.CpuUsage).Take(15);
+
+            foreach (var process in processesSorted)
+            {
+                string data =
+                    $"{process.Id}\t{process.ProcessName}\t{process.CpuUsage:0.#}%\t{ProcessMod.FormatMemSize(process.WorkingSet64)}";
+                output.Add(data);
+            }
+            
+            OnResult?.Invoke(this, output);
+        }   
+    }
+}
