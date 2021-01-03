e547b5d0a14a6d8ab6896180642f387ca45d033c
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-01-03 23:03:42 +0100

    Refactor the code

--- TestGtk/Model/ProcessMod.cs
+++ TestGtk/Model/ProcessMod.cs
@@ -25,6 +25,10 @@ namespace TestGtk.Model
             
         }
 
+        /// <summary>
+        /// A method that manages grabbing data from all processes.
+        /// </summary>
+        /// <returns>An array of ProcessMod objects</returns>
         public static ProcessMod[] GetProcesses()
         {
             Process[] processes = Process.GetProcesses();
@@ -34,32 +38,45 @@ namespace TestGtk.Model
             
             List<Task<ProcessMod>> tasks = new List<Task<ProcessMod>>(processes.Length);
             
+            // Load the list 'tasks' with the async method 'GetCpuUsageAsync' for each process
             foreach (var process in processes)
             {
                 tasks.Add(GetCpuUsageAsync(process));
             }
 
+            // Launch all tasks asynchronously
             Task task = Task.WhenAll(tasks);
+            
+            // Wait until all tasks finished
             task.Wait();
 
+            // Load the list 'proc' with 'ProcessMod' objects returned by each task
             for (int i = 0; i < tasks.Count; i++)
             {
                 proc.Add(tasks[i].Result);
             }
 
+            // Cleaning up
             foreach (var process in processes)
             {
                 process.Dispose();
             }
-
+            
             return proc.ToArray();
         }
 
+        /// <summary>
+        /// Get data about every process and calculate CPU usage in %.
+        /// All grabbed data are stored in a new instance of the ProcessMod class.
+        /// </summary>
+        /// <param name="proc">An instance of the Process class</param>
+        /// <returns>An instance of the Task class that returns a ProcessMod object</returns>
         private static async Task<ProcessMod> GetCpuUsageAsync(Process proc)
         {
             DateTime startTime = DateTime.UtcNow;
             var startCpuUsage = proc.TotalProcessorTime;
             
+            // Wait 500 ms
             await Task.Delay(500);
         
             DateTime endTime = DateTime.UtcNow;
@@ -86,11 +103,21 @@ namespace TestGtk.Model
             return tmp;
         }
 
+        /// <summary>
+        /// Format CPU usage
+        /// </summary>
+        /// <param name="cpuUsage">CPU usage in %, but not formatted</param>
+        /// <returns>Formatted CPU usage suffixed with percent character.</returns>
         public static string FormatCpuUsage(double cpuUsage)
         {
             return $"{cpuUsage:0.#} %";
         }
         
+        /// <summary>
+        /// Format memory usage
+        /// </summary>
+        /// <param name="size">Memory usage in bytes</param>
+        /// <returns>Formatted memory usage suffixed with appropriate unit.</returns>
         public static string FormatMemSize(double size)
         {
             double d = (double)size;
@@ -101,20 +128,30 @@ namespace TestGtk.Model
                 i++;
             }
             string[] unit = { "B", "KB", "MB", "GB", "TB" };
-            return $"{Math.Round(d, 2)} {unit[i]}";
+            return $"{Math.Round(d, 2).ToString(CultureInfo.CurrentCulture)} {unit[i]}";
             //return (string.Format("{0} {1}", Math.Round(d, 2), unit[i]));
         }
         
+        /// <summary>
+        /// Format the start time of the process
+        /// </summary>
+        /// <param name="ticks">Total number of ticks that passed during process launch</param>
+        /// <returns>Properly formatted time</returns>
         public static string FormatTime(long ticks)
         {
             DateTime dateTime = new DateTime(ticks);
 
-            return dateTime.ToString("G", CultureInfo.CreateSpecificCulture("de-DE"));
+            return dateTime.ToString("G", CultureInfo.CreateSpecificCulture("pl-PL"));
         }
         
+        /// <summary>
+        /// Format the CPU time
+        /// </summary>
+        /// <param name="size">Raw CPU time in ms</param>
+        /// <returns>Formatted time suffixed with 'ms'</returns>
         public static string FormatTimeMs(double size)
         {
-            return $"{size} ms";
+            return $"{size.ToString()} ms";
         }
     }
 }
